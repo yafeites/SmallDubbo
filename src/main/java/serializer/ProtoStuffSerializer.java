7d0a9f38e2dbd457acd6635fabfb79c33dc4c4aa
yafeites <myrljw@163.com>
2020-02-23 21:44:29 +0800

    增添与序列化相关的类

---
+++ src/main/java/serializer/ProtoStuffSerializer.java
@@ -0,0 +1,55 @@
+package serializer;
+
+
+import com.dyuproject.protostuff.LinkedBuffer;
+import com.dyuproject.protostuff.ProtostuffIOUtil;
+import com.dyuproject.protostuff.Schema;
+import com.dyuproject.protostuff.runtime.RuntimeSchema;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+//ProtoStuff序列化协议:要求序列化对象提供无参构造函数
+public class ProtoStuffSerializer implements Serializer {
+//      缓存得到的纲要以便下次直接用
+    private static Map<Class<?>, Schema<?>> cachedSchema = new ConcurrentHashMap<Class<?>, Schema<?>>();
+
+    @SuppressWarnings("unchecked")
+    private static <T> Schema<T> getSchema(Class<T> cls) {
+        Schema<T> schema = (Schema<T>) cachedSchema.get(cls);
+        if (null == schema) {
+            schema = RuntimeSchema.createFrom(cls);
+            cachedSchema.put(cls, schema);
+        }
+        return schema;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> byte[] serialize(T obj) {
+        Class<T> cls = (Class<T>) obj.getClass();
+        LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+        try {
+            Schema<T> schema = getSchema(cls);
+            return ProtostuffIOUtil.toByteArray(obj, schema, buffer);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            buffer.clear();
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T deserialize(byte[] data, Class<T> cls) {
+        try {
+            T message = (T) cls.getConstructors()[0].newInstance();
+            Schema<T> schema = getSchema(cls);
+            ProtostuffIOUtil.mergeFrom(data, message, schema);
+            return message;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
